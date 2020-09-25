9019b09f74a7fac5f602e709ef1252130047e95b
selkasse <selkasse@gmu.edu>
2020-09-25 19:36:21 -0400

    add code to reload component on window resize so that the grid inspector remains on top of the inventory grid. Also updated comments to fit Better Comments colors

--- src/components/InventoryGrid.js
+++ src/components/InventoryGrid.js
@@ -1,32 +1,44 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
-import InventoryCell from './InventoryCell';
 
 class InventoryGrid extends Component {
 
-    // TODO: add an event listener to re-render on window resize, as the grid inspector needs to be repositioned
+    //? *******************************************************************
+    //* Force a reload when the window resizes
+    //* This is so that the grid inspector resizes to fit the inventory
+    //? *******************************************************************
+    resize = () => {
+        this.forceUpdate()
+        window.location.reload();
+    }
+    //? *******************************************************************
+    //* -- End force reload code
+    //? *******************************************************************
 
     getName(item) {
         return item.includes("[") ? item.match(/\[(.*)\]/)[1].trim() : null;
     }
 
+    //? *******************************************************************
+    //* Taken from Dave Geddes: https://mastery.games/post/grid-inspector/
+    //? *******************************************************************    
     parseGridTemplate(templateStr, gap = "0px") {
         const gapSize = parseFloat(gap.replace("px", ""));
 
-        // splits at and space that isn't between two [ ] brackets
+        //*  splits at and space that isn't between two [ ] brackets
         const parsedArray = templateStr.split(/\s(?![^[]*])/);
         let lines = [];
         let currentPosition = 0;
 
-        // add in any missing names as ""
-        // "200px [center-start] 300px [center-end] 400px" becomes:
-        // "'' 200px [center-start] 300px [center-end] 400px ''"
+        //* add in any missing names as ""
+        //* "200px [center-start] 300px [center-end] 400px" becomes:
+        //* "'' 200px [center-start] 300px [center-end] 400px ''"
 
         let allItems = [];
 
         parsedArray.forEach((item, index) => {
             if (item.includes("px")) {
-                // add a null name before it if missing
+                //* add a null name before it if missing
                 if (!parsedArray[index - 1] || parsedArray[index - 1].includes("px")) {
                     allItems.push("");
                 }
@@ -35,7 +47,7 @@ class InventoryGrid extends Component {
             allItems.push(item);
         });
 
-        // handle last item
+        //* handle last item
         if (parsedArray[parsedArray.length - 1].includes("px")) {
             allItems.push("");
         }
@@ -50,8 +62,8 @@ class InventoryGrid extends Component {
                     end: currentPosition + 1,
                     name: this.getName(item)
                 };
-                // add gaps on just the inner lines
-                // and count it for positioning
+                //* add gaps on just the inner lines
+                //* and count it for positioning
                 if (index !== 0 && index !== allItems.length - 1 && gapSize !== 0) {
                     newLine.gap = gapSize;
                     currentPosition += gapSize;
@@ -66,6 +78,11 @@ class InventoryGrid extends Component {
 
     componentDidMount() {
 
+        window.addEventListener('resize', this.resize)
+
+        //? *******************************************************************
+        //* Taken from Dave Geddes: https://mastery.games/post/grid-inspector/
+        //? ******************************************************************* 
         const container = ReactDOM.findDOMNode(this);
         console.log(container);
         const inspector = document.createElement('div');
@@ -87,16 +104,16 @@ class InventoryGrid extends Component {
         const rowGap = styles.getPropertyValue('grid-row-gap')
         const columnGap = styles.getPropertyValue('grid-column-gap')
 
-        // convert the rows and columns into a format we can work with
+        //* convert the rows and columns into a format we can work with
         const columnData = this.parseGridTemplate(columns, columnGap)
         const rowData = this.parseGridTemplate(rows, rowGap)
 
         console.log(columnData, rowData);
 
-        // extend the lines a bit so we can see them better
+        //* extend the lines a bit so we can see them better
         const extendLines = 20
 
-        // render a line for each vertical grid line
+        //* render a line for each vertical grid line
         columnData.forEach(item => {
             const line = document.createElement('div')
             line.setAttribute(
@@ -112,7 +129,7 @@ class InventoryGrid extends Component {
             inspector.appendChild(line)
         })
 
-        // render a line for each horizontal grid line
+        //* render a line for each horizontal grid line
         rowData.forEach(item => {
             const line = document.createElement('div')
             line.setAttribute(
@@ -130,7 +147,9 @@ class InventoryGrid extends Component {
 
     }
 
- 
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.resize)
+    }
     render() {
         return (
             <div className='inventory'>
