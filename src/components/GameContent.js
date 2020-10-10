cc1fd76f1db540bb7003e23dce1b422dec67c805
selkasse <selkasse@gmu.edu>
2020-10-09 20:45:37 -0400

    fixed grid jumping when moving scroll in mobile view. moving on to fixing console size

--- src/components/GameContent.js
+++ src/components/GameContent.js
@@ -10,13 +10,13 @@ class GameContent extends Component {
                 <div className="top">
                     <p className="story">You found a muthafuckin scroll!</p><br />
                     <img className="story-img scroll" src="https://via.placeholder.com/150?text=2x2+scroll" alt="scroll" />
+                    <p>Use the code editor to move the scroll into your inventory</p>
+                    {/* <div className="help-text">
+                    </div> */}
                 </div>
 
                 <div className="bottom">
                     <InventoryGrid />
-                    <div className="help-text">
-                        <p>Use the code editor to move the scroll into your inventory</p>
-                    </div>
                 </div>
                 
             </div>
