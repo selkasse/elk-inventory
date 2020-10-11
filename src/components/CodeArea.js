840e7614d0d0088953b87031071076a6727ffbe5
selkasse <selkasse@gmu.edu>
2020-10-10 21:27:23 -0400

    remove old class component code from CodeArea.js

--- src/components/CodeArea.js
+++ src/components/CodeArea.js
@@ -10,13 +10,6 @@ import '../App.css';
 
 
 function CodeArea() {
-    // const [logs, setLogs] = useState([])
-
-    // run once!
-    // useEffect(() => {
-    //     Hook(window.console, log => setLogs(currLogs => [...currLogs, log]), false)
-    //     return () => Unhook(window.console)
-    // }, [])
 
     const onChange = (newValue) => {
         try {
@@ -26,11 +19,7 @@ function CodeArea() {
             try {
                 const results = userInput();
                 if (results) {
-                    // return <Console logs={this.state.logs} variant="dark" />
-
                     return <h1>hi</h1>
-
-                    // console.log(results);
                 }
                 this.forceUpdate()
                 window.location.reload();
@@ -134,118 +123,9 @@ function moveItem(row, col){
                 }}
             />
             <Console />
-            {/* <Console logs={logs} variant="dark" /> */}
         </div>
     )
 }
 
-
-// class CodeArea extends Component {
-
-   
-
-
-
-//     editorValue = 
-// `function validInput(row, col){
-//     if(!row || ! col) return false;
-
-//     const MAX_ROWS = 5;
-//     const MAX_COLS = 12;
-    
-//     return row <= MAX_ROWS && col <= MAX_COLS;
-// }
-
-// function getRandomInt(max){
-//     return Math.floor(Math.random() * Math.floor(max));
-// }
-
-// function moveItem(row, col){
-
-//     const item = document.querySelector('.story-img');
-
-//     const inventory = document.querySelector('.inventory');
-
-//     item.classList.remove('.story-img');
-
-
-//     if(validInput(row,col)){
-//         let style = document.createElement('style');
-//         style.type = 'text/css';
-
-//         const idFromClass = item.classList[1] + getRandomInt(1000);
-
-//         style.innerHTML = 
-//             \`#\${idFromClass} {
-//                 grid-column-start: \${col};
-//                 grid-row-start: \${row};
-//             }\`;
-
-//         document.getElementsByTagName('head')[0].appendChild(style);
-        
-//         item.id = idFromClass;
-        
-//         inventory.insertAdjacentElement('beforeEnd', item); 
-//     }
-    
-// }`;
-
-//     render() {
-
-//         function onChange(newValue) {
-//             try {
-//                 const userInput = new Function(newValue);
-
-//                 try {
-//                     const results = userInput();
-//                     if(results){
-//                         // return <Console logs={this.state.logs} variant="dark" />
-
-//                         return <h1>hi</h1>
-
-//                         // console.log(results);
-//                     }
-//                     this.forceUpdate()
-//                     window.location.reload();
-//                 }
-//                 catch(err){
-
-//                 }
-//             }
-//             catch(err){
-
-//             }
-//         }
-
-//         function onLoad(editor) {
-//             editor.getSession().setUseWrapMode(true);
-//             editor.session.foldAll();
-//         }
-
-//         return (
-//             <div className="code-area">
-//             <AceEditor
-//                 mode="javascript"
-//                 theme="dracula"
-//                 value={this.editorValue}
-//                 onChange={onChange}
-//                 onLoad={onLoad}
-//                 name="UNIQUE_ID_OF_DIV"
-//                 width="auto"
-//                 height="80%"
-//                 showPrintMargin={false}
-//                 fontSize="12px"
-//                 editorProps={{
-//                     $blockScrolling: Infinity }}
-//                 setOptions={{
-//                     enableBasicAutocompletion: true,
-//                     enableLiveAutocompletion: true,
-//                     enableSnippets: true
-//                 }}
-//             />    
-//             </div>
-//         )
-//     }
-// }
 
 export default CodeArea;
